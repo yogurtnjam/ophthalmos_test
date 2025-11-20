9a8a6baf4134be0b03f2137d94060ddfe113a878
Youmi Ji <youmi.ji.private@gmail.com>
2025-11-20 13:50:54 -0500

    Refactor server.listen to use simpler syntax
    Simplified server listening configuration and updated log message.

--- server/index.ts
+++ server/index.ts
@@ -71,11 +71,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  server.listen(port, () => {
+  log(`serving on http://localhost:${port}`);
+});
 })();
